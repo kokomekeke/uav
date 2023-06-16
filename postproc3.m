4f617a4f1df82f250a12c98178fd639e10d4679a
Aron Szabo <aron@szaboaron.hu>
2023-06-16 14:48:37 +0200

    Fix discontinuities on dfclient octave plots

--- postproc3.m
+++ postproc3.m
@@ -3,7 +3,13 @@ pkg load matgeom
 pkg load statistics
 pkg load signal
 
+index = (1:length(ahrs))
 ahrs_n = ahrs + (90.0/180.0)*pi;
+df_n = df;
+
+
+
+
 for f=1:length(ahrs_n)
   if ahrs_n(f)>pi
     ahrs_n(f) = ahrs_n(f) - 2*pi;
@@ -12,13 +18,24 @@ for f=1:length(ahrs_n)
     ahrs_n(f) = ahrs_n(f) + 2*pi;
   end
 end
+
+ahrs_n = ahrs_n(:).'; df_n = df_n(:).'; index = index(:).';
+discontinuities = (abs(diff(ahrs_n))>pi | abs(diff(df_n))>pi);
+ahrs_n = [ahrs_n; nan(1,length(ahrs_n))];
+df_n = [df_n; nan(1,length(df_n))];
+index = [index; nan(1,length(index))];
+ahrs_n(2*find(~discontinuities)) = [];
+df_n(2*find(~discontinuities)) = [];
+index(2*find(~discontinuities)) = [];
+ahrs_n = ahrs_n(:).'; df_n = df_n(:).'; index = index(:).';
+
 figure; title("Compass and DF measurement");
 hold on;
 #plot (phases(:, 1), "LineWidth", 2, "Color", "#AAFFAA");
 #plot (phases(:, 2), "LineWidth", 2, "Color", "#AAAAFF");
 #plot (phases(:, 3), "LineWidth", 2, "Color", "#FFAAFF");
-plot (ahrs_n*180/pi, "m");
-plot (df*180/pi, "b");
+plot (index, ahrs_n*180/pi, "m");
+plot (index, df_n*180/pi, "b");
 hold off;
 yticks(-180:10:180);
 grid on;
@@ -29,7 +46,7 @@ xlabel("Time (samples)");
 ylabel("Azimuth (deg)");
 
 figure;
-plot (ahrs_n*180/pi, df*180/pi, "m");
+plot (ahrs_n*180/pi, df_n*180/pi, "m");
 xlabel("Compass sensor (deg)");
 ylabel("DF Azimuth (deg)");
 xticks(-180:20:180);
