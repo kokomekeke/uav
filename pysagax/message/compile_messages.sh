e303737d6aea01e36c7957127fd7b496ba90cc29
Benedek Szakali <benedek.szakali@sagaxcommunications.com>
2024-02-12 16:27:16 +0100

    Added streaming control commands. Removed aggregate message type (Package) in data.proto

---
+++ pysagax/message/compile_messages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sources=("command.proto" "data.proto")
+target=$(dirname "${BASH_SOURCE[0]}")
+
+for i in "${sources[@]}"
+do
+    echo "Compiling $i"
+
+    echo "  Running protoc"
+    protoc --proto_path=$target --python_out=$target $i
+
+    echo "  Running protoletariat"
+    protol --create-package --in-place --python-out=$target protoc --proto-path=$target $i
+done
