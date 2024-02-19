3c811e4364a862cfb3da95f0683703a801f69515
Benedek Szakali <benedek.szakali@sagaxcommunications.com>
2024-02-19 15:00:27 +0100

    Added to_message method to DataType ENUM

--- pysagax/message/data_types.py
+++ pysagax/message/data_types.py
@@ -8,10 +8,25 @@ class DataType(Enum):
     EVENT = "v"
     ERROR = "e"
 
+    def to_message(self) -> Telemetry | Measurement | Event | OperationalError:
+        """Returns an empty Protobuf Message of the corresponding type"""
+
+        match self:
+            case self.TELEMETRY:
+                return Telemetry()
+            case self.MEASUREMENT:
+                return Measurement()
+            case self.EVENT:
+                return Event()
+            case self.ERROR:
+                return OperationalError()
+
     @classmethod
     def from_message(cls,
             message: Telemetry | Measurement | Event | OperationalError
     ) -> "DataType":
+        """Returns corresponding ENUM value based on Protobuf Message type"""
+
         match message:
             case Telemetry():
                 return cls.TELEMETRY
