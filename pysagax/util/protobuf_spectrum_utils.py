3bf43cae22a1bd7c2df25f5892a253cf19e026b2
Péter Rigó <peter.rigo@sagaxcommunications.com>
2024-05-13 17:26:19 +0200

    Spectrum utils: apply roi on spectrum

--- pysagax/util/protobuf_spectrum_utils.py
+++ pysagax/util/protobuf_spectrum_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pysagax.message.data_pb2 as proto_data
+import pysagax.message.command_pb2 as proto_cmd
 
 from typing import ValuesView
 
@@ -48,3 +49,20 @@ def create_spectrum_with_freq_dict(
     spectrum_with_freq = {f: a for f, a in list(zip(bin_freqs, spectrum_data))}
 
     return spectrum_with_freq
+
+
+def apply_roi_on_spectrum(
+    spectrum: proto_data.Spectrum | dict[float, float | int], roi: proto_cmd.ROIMask
+) -> tuple[dict[float, float | int], dict[float, float | int]]:
+    """
+    Separating spectrum (or spectrum with freq dict) to signal (inside the roi) and noise (outside the roi) bins
+    """
+    if isinstance(spectrum, proto_data.Spectrum):
+        spectrum = create_spectrum_with_freq_dict(spectrum)
+
+    roi_min = roi.center_frequency - roi.span / 2
+    roi_max = roi.center_frequency + roi.span / 2
+    signal_bins = {f: a for f, a in spectrum.items() if roi_min <= f and f <= roi_max}
+    noise_bins = {f: a for f, a in spectrum.items() if (roi_min > f or f > roi_max)}
+
+    return signal_bins, noise_bins
--- tests/util/test_protobuf_spectrum_utils.py
+++ tests/util/test_protobuf_spectrum_utils.py
@@ -3,8 +3,10 @@ from pysagax.util.protobuf_spectrum_utils import (
     protobuf_spectrum_to_numpy,
     convert_iterable_to_spectrum_data,
     create_spectrum_with_freq_dict,
+    apply_roi_on_spectrum,
 )
 import pysagax.message.data_pb2 as proto_data
+import pysagax.message.command_pb2 as proto_cmd
 import numpy as np
 
 
@@ -122,3 +124,74 @@ def test_create_spectrum_with_freq_dict_empty_spectrum():
     s = proto_data.Spectrum()
     spectrum_with_freqs = create_spectrum_with_freq_dict(s)
     assert spectrum_with_freqs == {}
+
+
+@pytest.mark.parametrize(
+    ["spectrum", "roi", "exp_signal_bins", "exp_noise_bins"],
+    [
+        [  # CASE 1:
+            {10: 0, 11: 1, 12: 2, 13: 3, 14: 4, 15: 5, 16: 6, 17: 7, 18: 8, 19: 9},
+            proto_cmd.ROIMask(center_frequency=15, span=5),
+            {13: 3, 14: 4, 15: 5, 16: 6, 17: 7},
+            {10: 0, 11: 1, 12: 2, 18: 8, 19: 9},
+        ],
+        [  # CASE 2:
+            {10: 0, 11: 1, 12: 2, 13: 3, 14: 4, 15: 5, 16: 6, 17: 7, 18: 8, 19: 9},
+            proto_cmd.ROIMask(center_frequency=15, span=1),
+            {15: 5},
+            {10: 0, 11: 1, 12: 2, 13: 3, 14: 4, 16: 6, 17: 7, 18: 8, 19: 9},
+        ],
+        [  # CASE 3: roi outside of spectrum:
+            {10: 0, 11: 1, 12: 2, 13: 3, 14: 4, 15: 5, 16: 6, 17: 7, 18: 8, 19: 9},
+            proto_cmd.ROIMask(center_frequency=5, span=5),
+            {},
+            {10: 0, 11: 1, 12: 2, 13: 3, 14: 4, 15: 5, 16: 6, 17: 7, 18: 8, 19: 9},
+        ],
+        [  # CASE4: roi larger than spectrum:
+            {10: 0, 11: 1, 12: 2, 13: 3, 14: 4, 15: 5, 16: 6, 17: 7, 18: 8, 19: 9},
+            proto_cmd.ROIMask(center_frequency=10, span=25),
+            {10: 0, 11: 1, 12: 2, 13: 3, 14: 4, 15: 5, 16: 6, 17: 7, 18: 8, 19: 9},
+            {},
+        ],
+        [  # CASE 5: roi span so small, no signal bins:
+            {10: 0, 11: 1, 12: 2, 13: 3, 14: 4, 15: 5, 16: 6, 17: 7, 18: 8, 19: 9},
+            proto_cmd.ROIMask(center_frequency=14.5, span=0.25),
+            {},
+            {10: 0, 11: 1, 12: 2, 13: 3, 14: 4, 15: 5, 16: 6, 17: 7, 18: 8, 19: 9},
+        ],
+        [  # CASE 6: bins at edge of roi should be signal bins:
+            {10: 0, 11: 1, 12: 2, 13: 3, 14: 4, 15: 5, 16: 6, 17: 7, 18: 8, 19: 9},
+            proto_cmd.ROIMask(center_frequency=15, span=2),
+            {14: 4, 15: 5, 16: 6},
+            {10: 0, 11: 1, 12: 2, 13: 3, 17: 7, 18: 8, 19: 9},
+        ],
+        [  # CASE 7: same as CASE 1 but with proto_data.Spectrum as input instead of a spectrum with freq dict
+            proto_data.Spectrum(
+                center_frequency=14.5,
+                bandwidth=9,
+                data=convert_iterable_to_spectrum_data(
+                    {
+                        10: 0,
+                        11: 1,
+                        12: 2,
+                        13: 3,
+                        14: 4,
+                        15: 5,
+                        16: 6,
+                        17: 7,
+                        18: 8,
+                        19: 9,
+                    }.values(),
+                    type=proto_data.Spectrum.DataType.INT16,
+                ),
+            ),
+            proto_cmd.ROIMask(center_frequency=15, span=5),
+            {13: 3, 14: 4, 15: 5, 16: 6, 17: 7},
+            {10: 0, 11: 1, 12: 2, 18: 8, 19: 9},
+        ],
+    ],
+)
+def test_apply_roi_on_spectrum(spectrum, roi, exp_signal_bins, exp_noise_bins):
+    signal_bins, noise_bins = apply_roi_on_spectrum(spectrum, roi)
+    assert signal_bins == exp_signal_bins
+    assert noise_bins == exp_noise_bins
