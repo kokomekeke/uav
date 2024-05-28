541155ef0eb3d6780e611f45b4d797848cbb8340
Aron Szabo <aron.szabo@sagaxcommunications.com>
2024-05-28 11:15:36 +0200

    Remove unneeded proxy def

--- pysagax/pysagax_uav.py
+++ pysagax/pysagax_uav.py
@@ -76,7 +76,6 @@ class Commander:
         self._post_proc_commands_q = self._manager.Queue()
         self._post_proc_responses_q = self._manager.Queue()
         self._post_proc_to_scan_engine_q = self._manager.Queue()
-        # self._latest_postproc_proxy = self._manager.dict()
         self._pp_heading_sync_input_q = self._manager.Queue()
         self._pp_detection_input_q = self._manager.Queue()
         self._pp_events_input_q = self._manager.Queue()
