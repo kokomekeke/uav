63cee4e741b501fe4a5748218543a288f8100acb
Péter Rigó <peter.rigo@sagaxcommunications.com>
2024-08-12 14:18:15 +0200

    black

--- pysagax/util/queue_put.py
+++ pysagax/util/queue_put.py
@@ -2,12 +2,13 @@ from queue import Queue
 import queue
 from typing import Any
 
-def queue_put(q: Queue[Any], data, timeout: float=1, logger = None, message:str = ""):
+
+def queue_put(q: Queue[Any], data, timeout: float = 1, logger=None, message: str = ""):
     """
     Tries to put data into q. If it's full, it drops the data.
     """
     try:
         q.put(data, timeout=timeout)
-    except queue.Full: 
+    except queue.Full:
         if logger is not None:
             logger.warning(" ".join("Output queue is full, data is dropped."))
