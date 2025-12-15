80d3a67159e904a3ff41a52da1e26381b278a5af
kokomekeke <72065311+kokomekeke@users.noreply.github.com>
2025-12-15 19:46:40 +0100

    Delete pysagax/heading directory

--- pysagax/heading/__init__.py
+++
@@ -1,9 +0,0 @@
-from pysagax.heading.heading_sources import (HeadingAHRS, HeadingAHRSUSB,
-                                             HeadingEncoder, HeadingStatic)
-
-__all__ = [
-    "HeadingStatic",
-    "HeadingEncoder",
-    "HeadingAHRS",
-    "HeadingAHRSUSB",
-]
